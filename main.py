74b052a1a029adf94e8fd589c26d48e5e2c4f024
Aditya Zagade <aditya1107zagade@gmail.com>
2026-08-27 01:33:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 import requests
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
-MODEL_NAME = "qwen2.5-coder:1.5b"
+MODEL_NAME = "qwen2.5-coder:3b"
 
 SCHEMA = {
     "type": "object",
